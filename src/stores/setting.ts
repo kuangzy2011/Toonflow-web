3e932eca0a70f052ff746ad0a2ceec84bbc253be
kuangzy1 <epay.kuangzy@126.com>
2026-08-19 13:46:19 +0800

    fix issue about allowing external devices to access website.

--- src/stores/setting.ts
+++ src/stores/setting.ts
@@ -6,7 +6,7 @@ export default defineStore(
     const canvasWheelEvent = ref("zoom");
     const activeMenu = ref("ui");
 
-    const baseUrl = ref<string>("http://localhost:10588/api");
+    const baseUrl = ref<string>("/api");
 
     const needUpdate = ref(false);
 
